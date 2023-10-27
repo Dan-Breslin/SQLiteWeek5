29768eb219a0dd7468fcb0790c3d4e47caa68e01
Breslin Danny <csf22brd>
2023-10-27 11:49:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sqliteweek5/DatabaseHelper.java
@@ -0,0 +1,21 @@
+package com.example.sqliteweek5;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public DatabaseHelper(@Nullable Context context)
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
