b5461396cce8bbe1a69b6f90bac2cc16cb25bf47
DBreslin <breslin-d5@ulster.ac.uk>
2023-10-27 12:06:05 +0100

    New updates

--- app/src/main/java/com/example/sqliteweek5/DatabaseHelper.java
+++ app/src/main/java/com/example/sqliteweek5/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.sqliteweek5;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,14 +9,28 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public DatabaseHelper(@Nullable Context context)
+    public DatabaseHelper(@Nullable Context context){
+        super(context, "Login.db", null, 1);
+
+    }
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create table user(email text primary key, password text) ");
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("Drop table if exists user");
+    }
 
+    public boolean insert(String email,String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email",email);
+        contentValues.put("password",password);
+        long ins = db.insert("user",null,contentValues);
+        if (ins ==-1) return false;
+        else return true;
     }
 }
